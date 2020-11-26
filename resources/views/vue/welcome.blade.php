6e536f9b32bdae6a2120b1487a6283c72e0e5f10
FranciscoSantosNeto <50747688+FranciscoSantosNeto@users.noreply.github.com>
2020-11-26 18:58:00 +0000

    home

--- resources/views/vue/welcome.blade.php
+++ resources/views/vue/welcome.blade.php
@@ -146,6 +146,13 @@
 </head>
 
 <body>
+
+<router-link to="/">Users</router-link>
+<router-link to="/departments">Department</router-link>
+
+<router-view></router-view>
+
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="{{ URL::asset('css/home.css') }}">
@@ -174,7 +181,7 @@ h1, h2, h3, h4, h5, h6 {
 <header class="w3-display-container w3-content w3-wide" style="max-width:600px;min-width:300px" id="home">
   <img class="w3-image" src="{{ URL::asset('home/hamburger.jpg') }}" alt="Hamburger Catering" width="1600" height="800">
   <div class="w3-display-bottomleft w3-padding-large w3-opacity">
-    <h1 class="w3-xxlarge">Le Catering</h1>
+    <h1 class="w3-xxlarge">Food@Home</h1>
   </div>
 </header>
 
