0cb03179ca343e7a59772424d09c105003e65963
Goncalo-sousa <52167323+Goncalo-sousa@users.noreply.github.com>
2020-12-03 23:14:25 +0000

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,55 +2,54 @@
 <html lan g="{{ config('app.locale') }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="{{ URL::asset('css/home.css') }}">
-    <title>@yield('title')</title>
-    
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-    @yield('extrastyles')
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+  @yield('extrastyles')
 </head>
 
 <style>
-body {font-family: "Times New Roman", Georgia, Serif;}
-h1, h2, h3, h4, h5, h6 {
-  font-family: "Playfair Display";
-  letter-spacing: 5px;
-}
+  body {
+    font-family: "Times New Roman", Georgia, Serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Playfair Display";
+    letter-spacing: 5px;
+  }
 </style>
 
-<body >
+<body>
   <div id="app">
-   </div>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ URL::asset('css/home.css') }}">
 
-   
-   
-  
-  </div>
-  
-
-
+    <!-- Navbar (sit on top) -->
+    <div class="w3-top">
+      <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+        <a class="w3-bar-item w3-button">Food@Home</a>
+        <div class="w3-right w3-hide-small">
+          <router-link class="w3-bar-item w3-button" to="/register">Register</router-link>
+        </div>
+      </div>
+    </div>
 
     <div class="container">
-        @yield('content')
+      @yield('content')
     </div>
     @yield('pagescript')
- </div>
+  </div>
 
-
-
-
-<!-- Footer -->
-<footer class="w3-center w3-light-grey w3-padding-32">
-  <p>Trabalho realizado em âmbito da UC desenvolvimento de aplicações distribuidas <a href="" target="_blank" class="w3-hover-text-green"></a></p>
-</footer>
-
-
+  </div>
 </body>
 
-<script src="{{ mix('js/app.js') }}"></script>
 </html>
--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -2,6 +2,14 @@
 
 @section('title', 'Food@Home')
 
+@section('extrastyles')
+<header class="w3-display-container w3-content w3-wide" style="max-width:600px;min-width:300px" id="home">
+  <img class="w3-image" src="{{ URL::asset('home/hamburger.jpg') }}" alt="Hamburger Catering" width="1600" height="800">
+  <div class="w3-display-bottomleft w3-padding-large w3-opacity">
+    <h1 class="w3-xxlarge">Food@Home</h1>
+  </div>
+</header>
+
 @section('content')
 
 <router-link to="/">Main</router-link>
