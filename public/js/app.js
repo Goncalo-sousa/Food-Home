61290c28f31c8bf682195380f74dcdbfe65cb12e
Goncalo-sousa <52167323+Goncalo-sousa@users.noreply.github.com>
2020-12-30 17:15:17 +0000

    update

--- public/js/app.js
+++ public/js/app.js
@@ -42286,8 +42286,8 @@ var Errors = /*#__PURE__*/function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\laragon\www\Food-Home\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\Food-Home\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
